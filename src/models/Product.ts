12810eb7fea2530f1eeac162fce3f7c2171d31ef
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2025-06-03 13:20:12 +0700

    Clothes

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,6 +1,7 @@
 import mongoose, { Document, Model, models, Schema, Types } from 'mongoose';
 
 export interface IProduct extends Document {
+  id: string;
   name: string;
   description: string;
   price: number;
@@ -22,18 +23,19 @@ const ProductSchema = new Schema<IProduct>(
       virtuals: true,
       versionKey: false,
       transform(_, ret) {
-        delete ret._id; 
+        ret.id = ret._id.toString();
+        delete ret._id;
       },
     },
   }
 );
 
+
 ProductSchema.virtual('id').get(function (this: { _id: Types.ObjectId }) {
   return this._id.toHexString();
 });
 
+
 const Product: Model<IProduct> = models.Product || mongoose.model<IProduct>('Product', ProductSchema);
 
 export default Product;
-
-
